7828cc4a171197422effcad3961f7921698c5b51
Peeter69 <dv237968@gmail.com>
2022-09-13 01:25:40 -0500

    init

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var logger = require('morgan');
+var express = require("express");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-const dbConnect = require('./database/db')
+var indexRouter = require("./routes/index");
+const dbConnect = require("./database/db");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
-dbConnect()
+dbConnect();
 
 module.exports = app;
